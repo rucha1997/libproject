089591b0341c380d0a5397eeee3aca53445db1c5
Gitahi Ng'ang'a <gitahi86@gmail.com>
2014-02-09 08:48:04 +0300

    Included GPL v3 licensing information.

--- src/main/java/org/itechkenya/leavemanager/jpa/ContractJpaController.java
+++ src/main/java/org/itechkenya/leavemanager/jpa/ContractJpaController.java
@@ -1,3 +1,21 @@
+/**
+ * LeaveManager, a basic leave management program for small organizations
+ * 
+ * Copyright (C) 2014 Gitahi Ng'ang'a
+ * 
+ * This file is part of LeaveManager.
+ * 
+ * LeaveManager is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * LeaveManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with LeaveManager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.itechkenya.leavemanager.jpa;
 
 import java.io.Serializable;
